99c7d7984f6b3fb88e95054ce653cd1dd7b150d7
Claudio87 <fuentealba_claudio@mail.ru>
2020-07-25 14:44:49 +0300

    printReadeableSize

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,15 @@ public class Main {
 
     public static void main(String[] args) {
         File file = new File(folderPath);
-        System.out.println(getFolderSize(file));
+        printReadeableSize(getFolderSize(file));
+//        System.out.println(getFolderSize(file));
+
     }
 
     static long getFolderSize(File folder){
         long totalSum = 0;
+
+
         if(folder.isFile()){
             return folder.length();
         }
@@ -18,6 +22,24 @@ public class Main {
         for(File f: list){
             totalSum += getFolderSize(f);
         }
+
         return totalSum;
     }
+
+    static void printReadeableSize(long size){
+        int index = Math.getExponent(size / 1024);
+        if (index < 10) {
+            double value = size / 1024;
+            System.out.printf("%.2f кБайт\n",value);
+        } if (index >= 10 && index < 20) {
+            double value = size / Math.pow(1024, 2);
+            System.out.printf("%.2f мБайт\n", value);
+        } if (index >= 20 && index < 30) {
+            double value = size / Math.pow(1024, 3);
+            System.out.printf("%.2f гБайт\n", value);
+        }
+
+    }
+
+
 }
